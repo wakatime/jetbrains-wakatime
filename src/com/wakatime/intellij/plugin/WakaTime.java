7d1fdd4e5fc368b51852f75148733290803bcd24
Alan Hamlett <alan.hamlett@gmail.com>
2016-02-07 08:28:58 -0800

    v6.0.4

--- src/com/wakatime/intellij/plugin/WakaTime.java
+++ src/com/wakatime/intellij/plugin/WakaTime.java
@@ -31,7 +31,7 @@ import org.apache.log4j.Level;
 
 public class WakaTime implements ApplicationComponent {
 
-    public static final String VERSION = "6.0.3";
+    public static final String VERSION = "6.0.4";
     public static final String CONFIG = ".wakatime.cfg";
     public static final long FREQUENCY = 2; // minutes between pings
     public static final Logger log = Logger.getInstance("WakaTime");
