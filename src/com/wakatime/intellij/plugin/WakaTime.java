a36a7f665d98c9f07526bb61b1afc93234be567b
Alan Hamlett <alan.hamlett@gmail.com>
2016-04-29 11:44:44 +0200

    v6.0.7

--- src/com/wakatime/intellij/plugin/WakaTime.java
+++ src/com/wakatime/intellij/plugin/WakaTime.java
@@ -36,7 +36,7 @@ import org.apache.log4j.Level;
 
 public class WakaTime implements ApplicationComponent {
 
-    public static final String VERSION = "6.0.6";
+    public static final String VERSION = "6.0.7";
     public static final String CONFIG = ".wakatime.cfg";
     public static final long FREQUENCY = 2; // max minutes between heartbeats, when continuously coding
     public static final Logger log = Logger.getInstance("WakaTime");
