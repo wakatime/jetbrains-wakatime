1af2896e8d69185027501ce3fa147f5a3c7ad5a8
Alan Hamlett <alan.hamlett@gmail.com>
2024-01-18 10:26:44 +0100

    Link to find api key easily from initial setup dialog prompt

--- src/com/wakatime/intellij/plugin/ApiKey.java
+++ src/com/wakatime/intellij/plugin/ApiKey.java
@@ -11,22 +11,35 @@ package com.wakatime.intellij.plugin;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.ValidationInfo;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.UUID;
 
 public class ApiKey extends DialogWrapper {
     private final JPanel panel;
+    private final JLabel label;
     private final JTextField input;
+    private final LinkPane link;
 
     public ApiKey(@Nullable Project project) {
         super(project, true);
         setTitle("WakaTime API Key");
         setOKButtonText("Save");
         panel = new JPanel();
+        panel.setLayout(new GridLayout(0,1));
+        label  = new JLabel("Enter your WakaTime API key:", JLabel.CENTER);
+        panel.add(label);
         input = new JTextField(36);
         panel.add(input);
+        link = new LinkPane("https://wakatime.com/api-key");
+        panel.add(link);
 
         init();
     }
@@ -60,4 +73,37 @@ public class ApiKey extends DialogWrapper {
         return input.getText();
     }
 
+}
+
+class LinkPane extends JTextPane {
+    private final String url;
+
+    public LinkPane(String url) {
+        this.url = url;
+        this.setEditable(false);
+        this.addHyperlinkListener(new UrlHyperlinkListener());
+        this.setContentType("text/html");
+        this.setBackground(new Color(0,0,0,0));
+        this.setText(url);
+    }
+
+    private class UrlHyperlinkListener implements HyperlinkListener {
+        @Override
+        public void hyperlinkUpdate(final HyperlinkEvent event) {
+            if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                try {
+                    Desktop.getDesktop().browse(event.getURL().toURI());
+                } catch (final IOException e) {
+                    throw new RuntimeException("Can't open URL", e);
+                } catch (final URISyntaxException e) {
+                    throw new RuntimeException("Can't open URL", e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void setText(final String text) {
+        super.setText("<html><body style=\"text-align:center;\"><a href=\"" + url + "\">" + text + "</a></body></html>");
+    }
 }
