062f8352a2e19dfccf832bfcf8f95769d1df8912
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-06-25 11:03:56 +0800

    fixes #283

--- src/com/wakatime/intellij/plugin/WakaTime.java
+++ src/com/wakatime/intellij/plugin/WakaTime.java
@@ -20,6 +20,7 @@ import com.intellij.openapi.application.ApplicationManager;
 //import com.intellij.openapi.compiler.CompilerTopics;
 import com.intellij.openapi.application.ApplicationNamesInfo;
 import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.diagnostic.LogLevel;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.*;
 import com.intellij.openapi.extensions.PluginId;
@@ -36,7 +37,7 @@ import com.intellij.openapi.wm.WindowManager;
 import com.intellij.util.concurrency.AppExecutorUtil;
 import com.intellij.util.messages.MessageBusConnection;
 import com.intellij.util.net.HttpConfigurable;
-import org.apache.log4j.Level;
+//import org.apache.log4j.Level;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -582,10 +583,10 @@ public class WakaTime implements ApplicationComponent {
     public static void setLoggingLevel() {
         try {
             if (WakaTime.DEBUG) {
-                log.setLevel(Level.DEBUG);
+                log.setLevel(LogLevel.DEBUG);
                 log.debug("Logging level set to DEBUG");
             } else {
-                log.setLevel(Level.INFO);
+                log.setLevel(LogLevel.INFO);
             }
         } catch(Throwable e) {
             System.out.println(e.getStackTrace());
